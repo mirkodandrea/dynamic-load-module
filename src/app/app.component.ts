799a1d0e21a2f6b8e686fb38a3513d57ef400f05
mirkodandrea <mirko.dandrea@gmail.com>
2020-11-06 14:51:58 +0100

    moved component registry to own file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,43 +4,9 @@ import {
   Injectable
 } from "@angular/core";
 import { DynamicComponent } from 'ng-dynamic-component';
+import { ComponentRegistry } from './component-registry.service';
 import { ModulesRegistry } from './dynamic-modules-registry';
 
-@Injectable({
-  providedIn: "root"
-})
-export class ComponentRegistry {
-  constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
-
-  public async get<T>(name: string) {
-    let moduleName = null;
-    if(name.includes('#')){
-      [moduleName, name] = name.split('#');
-    }
-    let factoryClasses;
-    if (moduleName == null) {
-      //@ts-ignore
-      factoryClasses = Array.from(this.componentFactoryResolver.ngModule._r3Injector.injectorDefTypes)
-        //@ts-ignore
-        .map(i => i.ɵmod.declarations)
-        //@ts-ignore
-        .flat();
-    } else {
-      const module = await ModulesRegistry[moduleName]();
-      factoryClasses = Array.from(module[moduleName].ɵmod.declarations)
-        //@ts-ignore
-      .flat();
-    }
-    const factoryClass = factoryClasses.find(m => m.key == name);
-
-    if (factoryClass === null) {
-      alert(`Component ${name} not found`);
-      return;
-    }
-    return factoryClass;
-
-  }
-}
 
 @Component({
   selector: "app-root",
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { DynamicModule } from "ng-dynamic-component";
-
+import { DynamicModule } from 'ng-dynamic-component';
+import { ComponentRegistry } from './component-registry.service';
 
 
 @NgModule({
@@ -13,7 +13,7 @@ import { DynamicModule } from "ng-dynamic-component";
     BrowserModule,
     DynamicModule
   ],
-  providers: [],
+  providers: [ComponentRegistry],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
